d91b74b706377de6a3e0ee72683db7376778b96e
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-02-20 15:05:02 -0500

    Add views and useEffect for GameList and GameDetail

---
+++ src/components/GameDetail.jsx
@@ -0,0 +1,97 @@
+
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router";
+
+function GameDetail(
+  {
+    // Props
+  }
+) {
+  // State declarations
+  const [targetGame, setTargetGame] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  // Functions
+  const { gameId } = useParams();
+
+  useEffect(() => {
+    fetch(process.env.API_SOURCE + "game/" + gameId, {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("Game fetch error");
+        }
+        return response.json();
+      })
+      .then((response) => setTargetGame(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  // Render
+  if (loading) return <p>Loading game info...</p>;
+  if (error) return <p>Network error, please try again later.</p>;
+  return (
+    <div className="mainDiv">
+      <Link to={"/"} className="mainLink">
+        Back
+      </Link>
+      <h1 className="pageHeader">Game Detail</h1>
+      <div className="blueBlock gameDetail">
+        <p className="gameTitle">{targetGame.title}</p>
+        <p>Ages {targetGame.ageRec}+ </p>
+        {targetGame.playerCtMax ? (
+          <p>
+            {targetGame.playerCtMin} - {targetGame.playerCtMax} players
+          </p>
+        ) : (
+          <p>{targetGame.playerCtMin} players</p>
+        )}
+
+        {targetGame.timeMax ? (
+          <p>
+            {targetGame.timeMin} - {targetGame.timeMax} min.
+          </p>
+        ) : (
+          <p>{targetGame.timeMin} min.</p>
+        )}
+        {targetGame.gameWeight == "Easy" && (
+          <p className="marker easy">{targetGame.gameWeight}</p>
+        )}
+        {targetGame.gameWeight == "Medium" && (
+          <p className="marker medium">{targetGame.gameWeight}</p>
+        )}
+        {targetGame.gameWeight == "Complex" && (
+          <p className="marker complex">{targetGame.gameWeight}</p>
+        )}
+        {targetGame.inCirc ? (
+          <p className=" available">Available</p>
+        ) : (
+          <p className=" unavailable">Unavailable</p>
+        )}
+        <p className="gameCardRow">{targetGame.desc}</p>
+
+        {targetGame.tags.length == 0 ? (
+          <p className="gameCardRow">No tags applied</p>
+        ) : (
+          <ul className="gameCardRow">
+            <p>Tags:</p>
+            {targetGame.tags.map((tag) => {
+              return (
+                <li key={tag.id}>
+                  <p>{tag.tagName}</p>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default GameDetail;
---
+++ src/components/GameList.jsx
@@ -0,0 +1,135 @@
+
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router";
+
+function GameList(
+  {
+    // Props
+  }
+) {
+  // State declarations
+  const [query, setQuery] = useState("");
+  const [gameList, setGameList] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Functions
+  useEffect(() => {
+    fetch(process.env.API_SOURCE + "game", {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("Game list fetch error");
+        }
+        return response.json();
+      })
+      .then((response) => setGameList(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+  };
+
+  async function submitQuery(e) {
+    e.preventDefault();
+    await fetch(process.env.API_SOURCE + `game/?title=${query}`, {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("Game list fetch error");
+        }
+        return response.json();
+      })
+      .then((response) => setGameList(response));
+  }
+
+  // Render
+  if (loading) return <p>Loading game list...</p>;
+  if (error) return <p>Network error, please try again later.</p>;
+
+  return (
+    <div className="mainDiv">
+      <h1 className="pageHeader">Game List</h1>
+      <form onSubmit={submitQuery} className="blueBlock">
+        <p>Search Games</p>
+        <label htmlFor="queryTag">Title:</label>
+        <input
+          type="text"
+          name="queryTag"
+          id="queryTag"
+          value={query}
+          onChange={handleQuery}
+        />
+        {/* TODO: Add addtl search params */}
+        {/* <label htmlFor="">Age Recommendation:</label>
+        <label htmlFor="">Min. Player Count:</label>
+        <label htmlFor="">Max. Player Count:</label>
+        <label htmlFor="">Complexity:</label> */}
+
+        <button>Search</button>
+      </form>
+
+      {gameList.length == 0 ? (
+        <h2 className="pageHeader">No games found</h2>
+      ) : (
+        <>
+          <h2 className="pageHeader">{gameList.length} games found:</h2>
+          <ul>
+            {gameList.map((game) => {
+              return (
+                <li key={game.id} className="blueBlock gameLI">
+                  <p className="gameTitle">{game.title}</p>
+                  <p>Ages {game.ageRec}+ </p>
+
+                  {game.playerCtMax ? (
+                    <p>
+                      {game.playerCtMin} - {game.playerCtMax} players
+                    </p>
+                  ) : (
+                    <p>{game.playerCtMin} players</p>
+                  )}
+
+                  {game.timeMax ? (
+                    <p>
+                      {game.timeMin} - {game.timeMax} min.
+                    </p>
+                  ) : (
+                    <p>{game.timeMin} min.</p>
+                  )}
+                  {game.gameWeight == "Easy" && (
+                    <p className="marker easy">{game.gameWeight}</p>
+                  )}
+                  {game.gameWeight == "Medium" && (
+                    <p className="marker medium">{game.gameWeight}</p>
+                  )}
+                  {game.gameWeight == "Complex" && (
+                    <p className="marker complex">{game.gameWeight}</p>
+                  )}
+
+                  {game.inCirc ? (
+                    <p className="available">Available</p>
+                  ) : (
+                    <p className="unavailable">Unavailable</p>
+                  )}
+                  <Link to={`/${game.id}`}>Detail</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default GameList;
