7ffc34d31ad341f761d9e8291598724314a35d34
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-02-27 13:25:43 -0500

    Add advanced search inputs to form

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router";
+import arrowDown from "../assets/arrowdown.svg";
+import arrowUp from "../assets/arrowup.svg";
 
 function GameList(
   {
@@ -7,14 +9,17 @@ function GameList(
   }
 ) {
   // State declarations
-  const [query, setQuery] = useState("");
+  const [qTitle, setQTitle] = useState("");
+  const [qGameWeight, setQGameWeight] = useState("");
+  const [qCount, setQCount] = useState("");
   const [gameList, setGameList] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   // Functions
   // PLACEHOLDER
-  const API_SOURCE = "https://game-library-api.fly.dev/";
+  // const API_SOURCE = "https://game-library-api.fly.dev/";
+  const API_SOURCE = "http://localhost:3000/";
 
   useEffect(() => {
     // fetch(import.meta.env.VITE_API_SOURCE + "game/circ", {
@@ -35,19 +40,42 @@ function GameList(
       .finally(() => setLoading(false));
   }, []);
 
-  const handleQuery = (e) => {
-    setQuery(e.target.value);
+  const handleQTitle = (e) => {
+    setQTitle(e.target.value);
+  };
+
+  const handleQGameWeight = (e) => {
+    setQGameWeight(e.target.value);
+  };
+
+  const handleQCount = (e) => {
+    setQCount(e.target.value);
+  };
+
+  const toggleAdvForm = () => {
+    const advSearch = document.querySelector(".advSearch");
+    advSearch.classList.toggle("hidden");
+    const icon = document.getElementById("expandIcon");
+    if (icon.src == arrowDown) {
+      icon.src = arrowUp;
+    } else if (icon.src == arrowUp) {
+      icon.src = arrowDown;
+    }
   };
 
   async function submitQuery(e) {
     e.preventDefault();
     // await fetch(import.meta.env.VITE_API_SOURCE + `game/?title=${query}`, {
-    await fetch(API_SOURCE + `game/?title=${query}`, {
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
+    await fetch(
+      API_SOURCE +
+        `game/circ/?title=${qTitle}&weight=${qGameWeight}&count=${qCount}`,
+      {
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
       .then((response) => {
         if (response.status >= 400) {
           throw new Error("Game list fetch error");
@@ -65,22 +93,88 @@ function GameList(
     <div className="mainDiv">
       <h1 className="pageHeader">Game List</h1>
       <form onSubmit={submitQuery} className="blueBlock">
-        <p>Search Games</p>
-        <label htmlFor="queryTag">Title:</label>
-        <input
-          type="text"
-          name="queryTag"
-          id="queryTag"
-          value={query}
-          onChange={handleQuery}
-        />
-        {/* TODO: Add addtl search params */}
-        {/* <label htmlFor="">Age Recommendation:</label>
-        <label htmlFor="">Min. Player Count:</label>
-        <label htmlFor="">Max. Player Count:</label>
-        <label htmlFor="">Complexity:</label> */}
+        <div className="formHeader">
+          <h2>Search</h2>
+          <div className="moreLabel">
+            <p>Advanced</p>
+            <img
+              src={arrowDown}
+              alt=""
+              id="expandIcon"
+              onClick={toggleAdvForm}
+            />
+          </div>
+        </div>
 
-        <button>Search</button>
+        <div className="formBody">
+          <label htmlFor="queryTag">Title:</label>
+          <input
+            type="text"
+            name="queryTag"
+            id="queryTag"
+            value={qTitle}
+            onChange={handleQTitle}
+          />
+          <div className="formBody advSearch hidden">
+            <label htmlFor="qPlayerCt">Player count:</label>
+            <input
+              type="number"
+              name="qPlayerCt"
+              id="qPlayerCt"
+              value={qCount}
+              onChange={handleQCount}
+            />
+            <fieldset className="diffField searchDiff">
+              <legend>Difficulty:</legend>
+              <div className="marker allRadio toggleSet">
+                <input
+                  type="radio"
+                  name="gameWeight"
+                  id="gameWeight1"
+                  value=""
+                  onChange={handleQGameWeight}
+                  defaultChecked={qGameWeight === ""}
+                />
+                <label htmlFor="gameWeight1">All</label>
+              </div>
+
+              <div className="marker easy toggleSet">
+                <input
+                  type="radio"
+                  name="gameWeight"
+                  id="gameWeight1"
+                  value="Easy"
+                  onChange={handleQGameWeight}
+                  defaultChecked={qGameWeight === "Easy"}
+                />
+                <label htmlFor="gameWeight1">Easy</label>
+              </div>
+              <div className="marker medium toggleSet">
+                <input
+                  type="radio"
+                  name="gameWeight"
+                  id="gameWeight2"
+                  value="Medium"
+                  onChange={handleQGameWeight}
+                  defaultChecked={qGameWeight === "Medium"}
+                />
+                <label htmlFor="gameWeight2">Med</label>
+              </div>
+              <div className="marker complex toggleSet">
+                <input
+                  type="radio"
+                  name="gameWeight"
+                  id="gameWeight3"
+                  value="Complex"
+                  onChange={handleQGameWeight}
+                  defaultChecked={qGameWeight === "Complex"}
+                />
+                <label htmlFor="gameWeight3">Comp</label>
+              </div>
+            </fieldset>
+          </div>
+          <button>Search</button>
+        </div>
       </form>
 
       {gameList.length == 0 ? (
