90a34ade973f531e1c0bef6170e446f22c0f1b96
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-03-06 17:02:14 -0500

    Add info to About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router";
 // import apiSource
 
 function About(
@@ -11,7 +12,20 @@ function About(
   // Render
   return (
     <div>
-      <p>About page</p>
+      <Link to={"/"} className="mainLink">
+        Back
+      </Link>
+      <div className="blueBlock">
+        <h2>About</h2>
+        <p>This is a fullstack app designed as a concept for use by a board game café. Guests may browse the collection and filter by game title, player count, difficulty, and genre. </p>
+        <a href="https://github.com/JakeBrowning90/game-library-public">
+          Github Repo & README
+        </a>
+        <p>Project by Jake Browning, 2025.</p>
+        <a href="https://jakebrowning90.github.io/personal-portfolio/">
+          My Portfolio
+        </a>
+      </div>
     </div>
   );
 }
